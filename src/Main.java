fe2fc535a74db926f63cf872038012e37caa805f
irvinebes <anya.shmakova.96@bk.ru>
2022-11-04 16:28:32 +0300

    Сделано ещё два задания

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,34 @@ public class Main {
         } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+        System.out.println("Задание 2.");
+        int clientDeviceYear = 2015;
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+
+
+        System.out.println("Задание 4.");
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+                System.out.println("Потребуется дней: " + 2);
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+                System.out.println("Потребуется дней: " + 3);
+        } else if (deliveryDistance > 100) {
+                System.out.println("Доставка будет занимать больше трёх дней.");
+        }
+
     }
 }
